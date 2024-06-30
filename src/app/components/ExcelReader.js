b11ea85fc5a383d3fc8d9988ff87a3827f3cd225
Maor <maorsa9@gmail.com>
2024-06-30 12:46:52 +0300

    read excel file with formattedDate

--- src/app/components/ExcelReader.js
+++ src/app/components/ExcelReader.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
+import Formulas from "./Formulas";
 
 const ExcelReader = () => {
   const [data, setData] = useState([]);
@@ -15,41 +16,48 @@ const ExcelReader = () => {
       const arrayBuffer = e.target.result;
       const workbook = XLSX.read(new Uint8Array(arrayBuffer), {
         type: "array",
+        cellDates: true, // Enable cellDates option
       });
 
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+        raw: false,
+        header: 1, // Read the first row for headers
+        range: 1, // Start reading from the second row
+      });
 
-      setData(jsonData);
-      if (jsonData.length > 0) {
-        setHeaders(Object.keys(jsonData[0]));
-      }
+      // Separate headers and data
+      const headers = jsonData[0];
+      const data = jsonData.slice(1);
+
+      // Format dates in the data
+      const formattedData = data.map((row) => {
+        const formattedRow = {};
+        row.forEach((cell, index) => {
+          const header = headers[index];
+          if (cell instanceof Date) {
+            formattedRow[header] = cell.toLocaleDateString();
+          } else {
+            formattedRow[header] = cell;
+          }
+        });
+        return formattedRow;
+      });
+
+      setData(formattedData);
+      setHeaders(headers);
     };
 
     reader.readAsArrayBuffer(file);
   };
 
-  const printCellValue = (rowIndex, colIndex) => {
-    if (data.length > rowIndex) {
-      const row = data[rowIndex];
-      const keys = Object.keys(row);
-      const keyAtIndex = keys[colIndex];
-
-      if (keyAtIndex !== undefined) {
-        console.log(
-          `Value at row ${rowIndex} and column ${colIndex} (${keyAtIndex}):`,
-          row[keyAtIndex]
-        );
-      } else {
-        console.log(`Column ${colIndex} not found in row ${rowIndex}`);
-      }
-    } else {
-      console.log(`Row ${rowIndex} not found`);
-    }
-  };
-
-  printCellValue(5, 2);
+  // const printAllValues = () => {
+  //   data.forEach((row) => {
+  //     console.log(row["שם "]);
+  //   });
+  // };
+  // printAllValues();
 
   return (
     <div className="flex flex-col items-center py-10">
@@ -59,7 +67,8 @@ const ExcelReader = () => {
         onChange={handleFileUpload}
         className="mb-4 p-2 border rounded-lg shadow-sm cursor-pointer"
       />
-      <div className="w-full max-w-6xl overflow-x-auto">
+      <Formulas data={data} />
+      {/* <div className="w-full max-w-6xl overflow-x-auto">
         <h3 className="text-xl font-semibold mb-4">Data from Excel file:</h3>
         {data.length > 0 ? (
           <table className="min-w-full bg-white border rounded-lg shadow-sm">
@@ -90,7 +99,7 @@ const ExcelReader = () => {
         ) : (
           <p>Add Excel File.</p>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
---
+++ src/app/components/Formulas.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Formulas = ({ data }) => {
+  console.log(data);
+  return <div>Formulas</div>;
+};
+
+export default Formulas;

de8f3a96eb9beef8b80cfab22330e7478a8fe5c8
Maor <maorsa9@gmail.com>
2024-06-30 13:40:40 +0300

    start working with some fomulas

--- src/app/components/Formulas.js
+++ src/app/components/Formulas.js
@@ -1,8 +1,24 @@
 import React from "react";
 
 const Formulas = ({ data }) => {
-  console.log(data);
-  return <div>Formulas</div>;
+  const startwork = data[3]?.["תאריך תחילת עבודה"];
+  const leave = data[3]?.["תאריך עזיבה"];
+
+  const getYearFromDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.getFullYear();
+  };
+
+  const startYear = startwork ? getYearFromDate(startwork) : null;
+  const leaveYear = leave ? getYearFromDate(leave) : null;
+
+  const yearDifference = startYear && leaveYear ? leaveYear - startYear : null;
+
+  console.log(`Start Year: ${startYear}`);
+  console.log(`Leave Year: ${leaveYear}`);
+  console.log(`Year Difference: ${yearDifference}`);
+
+  return <div>{`Year Difference: ${yearDifference}`}</div>;
 };
 
 export default Formulas;
